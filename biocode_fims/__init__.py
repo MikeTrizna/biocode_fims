de1aabe3473a4d4bf36d35e783e0b3b62a324f07
Mike Trizna <triznam@si.edu>
2017-06-29 10:14:30 -0400

    Bump version: 0.1.5 → 0.2.0

--- biocode_fims/__init__.py
+++ biocode_fims/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Mike Trizna"""
 __email__ = 'mike.trizna@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.2.0'
 
 from .biocode_fims import *
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='biocode_fims',
-    version='0.1.5',
+    version='0.2.0',
     description="A Python client for accessing data from the Biocode FIMS database.",
     long_description=readme + '\n\n' + history,
     author="Mike Trizna",
